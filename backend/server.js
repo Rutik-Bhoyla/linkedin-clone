49f5c25c5f98aba300b08deaac08eb2594adcdfc
Rutik <bhoylarutik7@gmail.com>
2025-11-02 16:51:56 +0530

    added mongo url to .env file and fixed minor problems

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const connectDB = require('./config/db')
+require('dotenv').config();
 
 const app = express()
+connectDB()
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> console.log(`Server running on ${PORT}`))
