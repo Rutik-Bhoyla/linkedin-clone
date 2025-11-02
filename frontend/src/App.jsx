08aa185f6e5bc3ba7632ebf739f3b6e305d763a8
Rutik <bhoylarutik7@gmail.com>
2025-11-02 23:44:16 +0530

    Added routes in app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,14 @@
+import { Route, Router, Routes } from "react-router-dom";
+
 const App = () => {
   return (
-    <div>App</div>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/feed" element={<Feed />} />
+      </Routes>
+    </Router>
   )
 }
 
