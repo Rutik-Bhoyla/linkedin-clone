1f996bc92c871c5ecb8843acd115d674b929d68e
Rutik <bhoylarutik7@gmail.com>
2025-11-02 23:31:26 +0530

    Created frontend folder structure

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,12 @@ require('dotenv').config();
 const app = express()
 connectDB()
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/posts', require('./routes/posts'));
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> console.log(`Server running on ${PORT}`))
