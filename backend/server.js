a9d745873ec1ec9be78b141b1d2d7136be5b0818
Rutik <bhoylarutik7@gmail.com>
2025-11-03 23:03:32 +0530

    server api fix

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/posts', require('./routes/posts'));
 
+app.get('/', (req,res)=>{
+    res.send("API IS WORKING")
+})
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> console.log(`Server running on ${PORT}`))
